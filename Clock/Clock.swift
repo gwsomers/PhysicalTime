3408692ec3176b3accd63de1bbf6d7d0b432e6f0
XiOuyang <XiWanyaohei@gmail.com>
2018-02-04 14:50:13 -0800

    Uploading clock tutorial as base for project

---
+++ Clock/Clock.swift
@@ -0,0 +1,16 @@
+//
+//  Clock.swift
+//  Clock
+//
+//  Created by Xi Stephen Ouyang on 1/31/18.
+//  Copyright © 2018 Your School. All rights reserved.
+//
+
+import Foundation
+
+class Clock {
+    
+    var currentTime: NSDate {
+        return NSDate()
+    }
+}
---
+++ Clock/ViewController.swift
@@ -0,0 +1,53 @@
+/*
+
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
+4.0 International License, by Yong Bakos.
+
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer: Timer?
+    let clock = Clock()
+    var test: Timer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("You fucking degenerate")
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.updateTimeLabel), name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimeLabel), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimeLabel() {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        timeLabel.text = "\(formatter.string(from: clock.currentTime as Date))"
+        timeLabel.textColor = UIColor.cyan
+    }
+    
+//    deinit {
+//        NotificationCenter.default.removeObserver(self)
+//    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTimeLabel()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        timer?.invalidate()
+    }
+}
+
