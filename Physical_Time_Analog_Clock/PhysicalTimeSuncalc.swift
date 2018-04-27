f23c8f48f1fad019236222ceb74e4db04b483eb5
cralgonz <cralgonz@ca-mac203.lt.ucsc.edu>
2018-04-26 21:18:25 -0700

    Changes to working state (Suncalc library)

--- Physical_Time_Analog_Clock/PhysicalTimeSuncalc.swift
+++ Physical_Time_Analog_Clock/PhysicalTimeSuncalc.swift
@@ -42,16 +42,16 @@ import Foundation
 class PhysicalTimeSuncalc
 {
     // Simple conventions
-    let PI: Double = Double.pi
-    let rad: Double = Double.pi / 180
+    internal let PI: Double = Double.pi
+    internal let rad: Double = Double.pi / 180
     
     // Date/time conversions (Julian time)
-    let dayMs: Double = 1000 * 60 * 60 * 24
-    let J1970: Double = 2440588.0
-    let J2000: Double = 2451545.0
+    internal let dayMs: Double = 1000 * 60 * 60 * 24
+    internal let J1970: Double = 2440588.0
+    internal let J2000: Double = 2451545.0
     
     // Obliquity of the Earth
-    let e: Double = (Double.pi / 180) * 23.4397
+    internal let e: Double = (Double.pi / 180) * 23.4397
 
     /**
      Conversion of the current date to Julian
@@ -60,7 +60,7 @@ class PhysicalTimeSuncalc
      - returns:
      A double calculating the date in Julian, in milliseconds
      */
-    func toJulian(date: Date) -> Double
+    private func toJulian(date: Date) -> Double
     {
         return Double(Date().millisecondsSince1970) / dayMs - 0.5 + J1970
     }
@@ -72,7 +72,7 @@ class PhysicalTimeSuncalc
      - returns:
      A date object for any specified Julian value
      */
-    func fromJulian(j: Double) -> Date
+    private func fromJulian(j: Double) -> Date
     {
         return Date(milliseconds: Int((j + 0.5 - J1970) * dayMs))
     }
@@ -84,7 +84,7 @@ class PhysicalTimeSuncalc
      - returns:
      A double value for the total amount of days.
      */
-    func toDays(date: Date) -> Double
+    private func toDays(date: Date) -> Double
     {
         return toJulian(date: date) - J2000
     }
@@ -97,11 +97,118 @@ class PhysicalTimeSuncalc
      - return:
     A double indicating the right acension given the appropriate function arguments
      */
-    func rightAscension(l: Double, b: Double) -> Double
+    private func rightAscension(l: Double, b: Double) -> Double
     {
         // TODO
 //        return atan(sin(l) * cos(e) - tan(b) * sin(e), cos(l))
         return 0.0
+    }
+    
+    /**
+     General calculation for positioning (declination)
+     - parameters:
+        - l: TODO
+        - b: TODO
+     - return:
+     A double indicating the declination given the appropriate function arguments
+     */
+    private func declination(l: Double, b: Double) -> Double
+    {
+        // TODO
+        return asin(sin(b) * cos(e) + cos(b) * sin(e) * sin(l))
+    }
+    
+    /**
+     The azimuth is the angle between a celestial body (sun, moon) and the North, measured clockwise around the
+     observer's horizon.
+     */
+    private func azimuth()
+    {
+        // TODO
+    }
+    
+    /**
+     Finding the altitude based on
+     - parameters:
+        - H: TODO
+        - phi: TODO
+        - dec: TODO
+     - return:
+    TODO
+     */
+    private func altitude(H: Double, phi: Double, dec: Double) -> Double
+    {
+        // TODO
+        return asin(sin(phi) * sin(dec) + cos(phi) * cos(dec) * cos(H));
+    }
+    
+    /**
+     Calculating the time reckoned from the motion of the earth relative to the distant stars.
+     - parameters:
+        - d: TODO
+        - lw: TODO
+     - return:
+     TODO
+     */
+    private func siderealTime(d: Double, lw: Double) -> Double
+    {
+        return rad * (280.16 + 360.9856235 * d) - lw;
+    }
+    
+    /**
+     Astronomical refraction deals with the angular position of celestial bodies, their appearance as a point source, and through differential refraction, the shape of extended bodies such as the Sun and Moon.
+     - parameters:
+        - h: Measure of degrees
+     - return:
+     Measure of astronomical refraction in radians
+     */
+    private func astroRefraction(h: Double) -> Double
+    {
+        var h: Double = h
+        // The following formula works for positive altitudes only. A div/0 will occur if less than 0 (hence the
+        // following conditional
+        if (h < 0)
+        {
+            h = 0.0;
+        }
+        
+        // See forumla 16.4 of "Astronomical Algorithms" 2nd edition by Jean Meeus (Willmann-Bell, Richmond) 1998
+        // 1.02 / tan(h + 10.26 / (h + 5.10)) h in degrees, result in arc minutes -> converted to radians
+        return 0.0002967 / tan(h + 0.00312536 / (h + 0.08901179))
+    }
+    
+    /**
+     The mean anomaly of the Sun (actually, of the Earth in its orbit around the Sun, but it is convenient to pretend the Sun orbits the Earth), is: g = 357.528deg + 0.9856003deg(n)
+     Can be used to calculate the apparent coordinates of the Sun, mean equinox and ecliptic of date.
+     - parameters:
+        - d: TODO
+     - return:
+     TODO
+     */
+    private func solarMeanAnomaly(d: Double) -> Double
+    {
+        return rad * (357.5291 + 0.98560028 * d)
+    }
+    
+    /**
+     TODO
+     */
+    private func eclipticLongitude(m: Double) -> Double
+    {
+        // Equation of the center
+        let c: Double = rad * (1.9148 * sin(m) + 0.02 * sin(2 * m) + 0.0003 * sin(3 * m))
+        // Perihelion of the Earth
+        let p: Double = rad * 102.9372
+        return m + c + p + PI
+    }
+    
+    /**
+     Obtaining the coordinates of the sun.
+     */
+    private func sunCoords(d: Double)
+    {
+        let mean = solarMeanAnomaly(d: d)
+        let long = eclipticLongitude(m: mean)
     }
     
     init()
