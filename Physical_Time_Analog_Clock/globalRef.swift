2894c773792ef02febf02b227116ea1099c37300
Erik Corey Lau <ercolau@unix1.lt.ucsc.edu>
2018-04-30 17:56:00 -0700

    store

---
+++ Physical_Time_Analog_Clock/globalRef.swift
@@ -0,0 +1,9 @@
+//
+//  globalRef.swift
+//  Physical_Time_Analog_Clock
+//
+//  Created by ercolau on 4/30/18.
+//  Copyright © 2018 Xi Stephen Ouyang. All rights reserved.
+//
+
+import Foundation
