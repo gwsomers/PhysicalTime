505c41a5b3c300d7a5e5e4200aeb1cb61e7141c5
Erik Lau <legionlords75@gmail.com>
2018-04-30 17:59:15 -0700

    stuffs

--- Physical_Time_Analog_Clock/WelcomeView.swift
+++ Physical_Time_Analog_Clock/WelcomeView.swift
@@ -18,9 +18,40 @@ class WelcomeView: UIViewController
      - returns:
      nil
      */
+    var defaulthour = 24
+    var defaultminute = 60
+    var defaultREVDAY = 2
+    var defaultREVHOUR = 0
+    var defaultANGOFF = 1
+    var defaultTIMEOFF = 0.0
+    var defaultMODE = 1
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage:UIImage(named: "morningGoldenHour.jpeg")!)
     }
+    @IBAction func checkIfNew(_ sender: UIButton) {
+        let file = "boring"
+        let defaultinfo = "24:60:2:0:1:0:1"
+        var recievedinfo=""
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+        let fileURL = dir.appendingPathComponent(file).appendingPathExtension("txt")
+        do {
+            recievedinfo = try String(contentsOf: fileURL,encoding: .utf8)
+            let WordsArray = recievedinfo.components(separatedBy: ":")
+            var count = 0;
+            for sect in WordsArray{
+                
+            }
+        }
+            catch{
+                do {
+                    try defaultinfo.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+                }
+                catch{}
+            }
+        }
+    }
+    
+    
 }
--- Physical_Time_Analog_Clock/globalRef.swift
+++
@@ -1,9 +0,0 @@
-//
-//  globalRef.swift
-//  Physical_Time_Analog_Clock
-//
-//  Created by ercolau on 4/30/18.
-//  Copyright © 2018 Xi Stephen Ouyang. All rights reserved.
-//
-
-import Foundation
