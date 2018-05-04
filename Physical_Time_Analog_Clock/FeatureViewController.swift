86fc85631b5d1047613c1d04decda4965fff90fa
XiOuyang <>
2018-05-04 13:08:37 -0700

    implemented dynamic changing color on buttons

--- Physical_Time_Analog_Clock/FeatureViewController.swift
+++ Physical_Time_Analog_Clock/FeatureViewController.swift
@@ -9,14 +9,55 @@
 import UIKit
 
 class FeatureViewController: UIViewController {
-
+    
+    @IBOutlet weak var lunarFeatureButton: UIButton!
+    @IBOutlet weak var wadokeiFeatureButton: UIButton!
+    @IBOutlet weak var planetaryVisButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let background = changeBackground()
         self.view.backgroundColor = UIColor(patternImage:UIImage(named: background.getBackground())!)
 
-
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let background = changeBackground()
+        self.view.backgroundColor = UIColor(patternImage:UIImage(named: background.getBackground())!)
+        
+        switch background.getBackground() {
+        case "sunrise.jpg":
+            lunarFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.orange, for: .normal);
+        case "morningGoldenHour.jpg":
+            lunarFeatureButton.setTitleColor(UIColor.yellow, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.yellow, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.yellow, for: .normal);
+        case "noon.jpg":
+            lunarFeatureButton.setTitleColor(UIColor.yellow, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.yellow, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.yellow, for: .normal);
+        case "sunset.jpg":
+            lunarFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.orange, for: .normal);
+        case "night.jpg":
+            lunarFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.orange, for: .normal);
+        case "lunar_pic.jog":
+            lunarFeatureButton.setTitleColor(UIColor.white, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.white, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.white, for: .normal);
+        default:
+            lunarFeatureButton.setTitleColor(UIColor.black, for: .normal);
+            wadokeiFeatureButton.setTitleColor(UIColor.black, for: .normal);
+            planetaryVisButton.setTitleColor(UIColor.black, for: .normal);
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- Physical_Time_Analog_Clock/MenuView.swift
+++ Physical_Time_Analog_Clock/MenuView.swift
@@ -13,6 +13,8 @@ import UIKit
  */
 class MenuView: UIViewController
 {
+    @IBOutlet weak var goToClockButton: UIButton!
+    @IBOutlet weak var goToFeatureButton: UIButton!
     /**
      Do any additional setup after loading the view (specifically, in our case, setting the background).
      - returns:
@@ -23,5 +25,38 @@ class MenuView: UIViewController
         super.viewDidLoad()
         let background = changeBackground()
         self.view.backgroundColor = UIColor(patternImage:UIImage(named: background.getBackground())!)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let background = changeBackground()
+        self.view.backgroundColor = UIColor(patternImage:UIImage(named: background.getBackground())!)
+        
+        switch background.getBackground() {
+        case "sunrise.jpg":
+            goToClockButton.setTitleColor(UIColor.orange, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+        case "morningGoldenHour.jpg":
+            goToClockButton.setTitleColor(UIColor.yellow, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.yellow, for: .normal);
+        case "noon.jpg":
+            goToClockButton.setTitleColor(UIColor.yellow, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.yellow, for: .normal);
+        case "sunset.jpg":
+            goToClockButton.setTitleColor(UIColor.orange, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+        case "night.jpg":
+            goToClockButton.setTitleColor(UIColor.orange, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.orange, for: .normal);
+        case "lunar_pic.jpg":
+            goToClockButton.setTitleColor(UIColor.white, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.white, for: .normal);
+            
+        default:
+            goToClockButton.setTitleColor(UIColor.black, for: .normal);
+            goToFeatureButton.setTitleColor(UIColor.black, for: .normal);
+        }
     }
 }
