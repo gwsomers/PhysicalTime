efe710e13da3f14fadd95f9cf866b5def04de2b8
Cristian Gonzales <xcristian.gonzales@gmail.com>
2018-05-29 04:59:18 -0700

    Changes to stats page

--- Physical Time/StatsViewController.swift
+++ Physical Time/StatsViewController.swift
@@ -20,7 +20,11 @@ class StatsViewController: UIViewController
     @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var textView: UITextView!
-
+    @IBOutlet weak var secondTextView: UITextView!
+    @IBOutlet weak var thirdTextView: UITextView!
+    @IBOutlet weak var fourthTextView: UITextView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     /**
      On load, dynamically change the background based on the time of day
      - returns:
@@ -50,10 +54,13 @@ class StatsViewController: UIViewController
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // Determine the text to set for our textView
-        self.textView.text = getDescription()
+        // Determine the text to set for all of our text views
+        self.textView.text = getTime()
+        self.secondTextView.text = getAltitude()
+        self.thirdTextView.text = getAzimuth()
+        self.fourthTextView.text = getMiscMoonData()
         
-        // Do the fades to both UI elements appropriately
+        // Do the fades to all UI elements appropriately
         self.menuButton.fadeIn(duration: 2.0, delay: 1.0, completion: {
             (finished: Bool) -> Void in
         })
@@ -63,13 +70,102 @@ class StatsViewController: UIViewController
         self.textView.fadeIn(duration: 2.0, delay: 1.0,  completion: {
             (finished: Bool) -> Void in
         })
+        self.pageControl.fadeIn(duration: 2.0, delay: 1.0,  completion: {
+            (finished: Bool) -> Void in
+        })
         
         // Create a background object
         let background = ChangeBackground()
-        // Set the font color for our text view
+        // Set the font color for our text views
         setFontColor(textView: textView, backgroundType: background.getBackground())
+        setFontColor(textView: secondTextView, backgroundType: background.getBackground())
+        setFontColor(textView: thirdTextView, backgroundType: background.getBackground())
+        setFontColor(textView: fourthTextView, backgroundType: background.getBackground())
     }
     
+    /**
+     Event listener for the page change on this view controller, which would indicate that
+     the user wishes to see the rest of the propagated data. We should switch accordingly.
+     We will be fading "by neighbors", if you will
+     
+     - parameters:
+     - sender: An event listener waiting for the page control to be changed, effectively
+        passing a UIPageControl element within the function body.
+     
+     - returns:
+     nil
+     */
+    @IBAction func pageChange(_ sender: UIPageControl)
+    {
+        // Switch statement to determine which text views to fade out and fade in, so
+        // the user may see all the propagated data.
+        switch (sender.currentPage)
+        {
+            // Fade into the first page and out of the second
+            case 0:
+                self.secondTextView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                    (finished: Bool) -> Void in
+                })
+                self.textView.fadeIn(duration: 0.5, delay: 0.5,  completion: {
+                    (finished: Bool) -> Void in
+                })
+            // Fade out of the first page or the third page and into the second
+            case 1:
+                // Conditional to see which UI element alpha value is currently 1
+                if thirdTextView.alpha == CGFloat(1.0)
+                {
+                    self.thirdTextView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                        (finished: Bool) -> Void in
+                    })
+                }
+                else
+                {
+                    self.textView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                        (finished: Bool) -> Void in
+                    })
+                }
+                // After the fade-out, fade into this second page
+                self.secondTextView.fadeIn(duration: 0.5, delay: 0.5,  completion: {
+                    (finished: Bool) -> Void in
+                })
+            
+            // Fade out of the second page or the fourth page and into the third page
+            case 2:
+                // Conditional to see which UI element alpha value is currently 1
+                if fourthTextView.alpha == CGFloat(1.0)
+                {
+                    self.fourthTextView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                        (finished: Bool) -> Void in
+                    })
+                }
+                else
+                {
+                    self.secondTextView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                        (finished: Bool) -> Void in
+                    })
+                }
+                // After the fade-out, fade into this second page
+                self.thirdTextView.fadeIn(duration: 0.5, delay: 0.5,  completion: {
+                    (finished: Bool) -> Void in
+                })
+            // Fade out of the third page and into the fourth page
+            case 3:
+                self.thirdTextView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                    (finished: Bool) -> Void in
+                })
+                self.fourthTextView.fadeIn(duration: 0.5, delay: 0.5,  completion: {
+                    (finished: Bool) -> Void in
+                })
+            // Simply make case 0 the default
+            default:
+                self.secondTextView.fadeOut(duration: 0.5, delay: 0.0,  completion: {
+                    (finished: Bool) -> Void in
+                })
+                self.textView.fadeIn(duration: 0.5, delay: 0.5,  completion: {
+                    (finished: Bool) -> Void in
+                })
+        }
+    }
     /**
      Helper function to set the font color of the text view based on the background
      color, that of which is determined by the time of day.
@@ -109,9 +205,10 @@ class StatsViewController: UIViewController
      a description to be dynamically loaded into the text view.
      
      - returns:
-     A string indicating what will be put in the
+     A string indicating what will be put in the view in terms of the different
+     times of the day.
      */
-    func getDescription() -> String
+    func getTime() -> String
     {
         // Declaring a new SwiftySuncalc object
         let suncalc: SwiftySuncalc! = SwiftySuncalc()
@@ -256,5 +353,94 @@ class StatsViewController: UIViewController
                 return "Oops! Something seems to be broken! Sorry about that! Please report this" + " bug at https://github.com/gwsomers/PhysicalTime. Thank you for your"
                     + " understanding!"
         }
+    }
+    
+    /**
+     Helper function to get the moon and sun altitude.
+     
+     - returns:
+     A formatted string with the moon's and sun's altitude.
+     */
+    func getAltitude() -> String
+    {
+        // Declare the string variable to be appended to
+        var stringBuilder: String!
+        // Declaring a new SwiftySuncalc object
+        let suncalc: SwiftySuncalc! = SwiftySuncalc()
+        // Today's date/time
+        let today: Date! = Date()
+        // Get the sun/moon positions and the moon phase
+        // TODO: Fix hardcoded latitude and longitude coordinates
+        let sunPos = suncalc.getPosition(date: today, lat: 37, lng: -122)
+        let moonPos = suncalc.getMoonPosition(date: today, lat: 37, lng: -122)
+        // Building the string based on the values returned in the three dictionaries
+        stringBuilder = "In the horizontal coordinate system, altitude is the " +
+                        "coordinate that measures the height above the horizon (in " +
+                        "degrees). The other coordinate is the azimuth. Because the true " +
+                        "horizon depends on the local landscape and the exact location of " +
+                        "the observer, astronomers often use an \"artificial\" horizon " +
+                        "that runs exactly midway between the zenith and nadir.\n" +
+                        "Today, the altitude of the sun is \(sunPos["altitude"]!)°" +
+                        ". The altitude of the moon is \(moonPos["altitude"]!)°" +
+                        ".\n\n"
+        // Return the "built" string
+        return stringBuilder
+    }
+    
+    /**
+     Helper function to get the moon and sun azimuth.
+     
+     - returns:
+     A formatted string with the moon's and sun's azimuth.
+     */
+    func getAzimuth() -> String
+    {
+        // Declare the string variable to be appended to
+        var stringBuilder: String!
+        // Declaring a new SwiftySuncalc object
+        let suncalc: SwiftySuncalc! = SwiftySuncalc()
+        // Today's date/time
+        let today: Date! = Date()
+        // Get the sun/moon positions
+        // TODO: Fix hardcoded latitude and longitude coordinates
+        let sunPos = suncalc.getPosition(date: today, lat: 37, lng: -122)
+        let moonPos = suncalc.getMoonPosition(date: today, lat: 37, lng: -122)
+        // Building the string based on the values returned in the three dictionaries
+        stringBuilder = "The azimuth is the coordinate from the horizontal coordinate " +
+                        "system that indicates the direction along the horizon. The " +
+                        "azimuth is measured in degrees, but not everyone uses the same " +
+                        "range of azimuth or the same zero point. Sometimes the azimuth " +
+                        "is measured between −180 and +180°, sometimes between 0 and 360°, " +
+                        "and sometimes with 0° in the south, and sometimes with 0° in the " +
+                        "north. For astronomical application it is convenient to set 0° " +
+                        "in the south and to measure azimuth between −180 and +180°: that " +
+                        "provides the best fit to the hour angle.\n" +
+                        "Today, the azimuth of the sun is \(sunPos["azimuth"]!)°. " +
+                        "The aziumth of the moon is \(moonPos["altitude"]!)°."
+        // Return the "built" string
+        return stringBuilder
+    }
+    
+    /**
+     Helper function to get miscellaneous moon data, such as the distance, parallactic angle,
+     and moon phase.
+     
+     - returns:
+     A formatted string with the miscellaneous moon data, as priorly mentioned.
+     */
+    func getMiscMoonData() -> String
+    {
+        // Declare the string variable to be appended to
+        var stringBuilder: String! = ""
+        // Declaring a new SwiftySuncalc object
+        let suncalc: SwiftySuncalc! = SwiftySuncalc()
+        // Today's date/time
+        let today: Date! = Date()
+        // Get the sun/moon positions and the moon phase
+        let moonPos = suncalc.getMoonPosition(date: today, lat: 37, lng: -122)
+        let moonIllumination = suncalc.getMoonIllumination(date: today)
+        
+        
+        return stringBuilder
     }
 }
